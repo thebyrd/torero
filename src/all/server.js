be8c36057b3cd563f80c16eab9f38f9a24ddc9d7
David Byrd <david@byrdhou.se>
2012-10-01 22:38:56 -0400

    added sessions & query parsing to server.js

--- src/all/server.js
+++ src/all/server.js
@@ -1,45 +1,52 @@
-var torero = require('torero')
-  , env = process.env.NODE_ENV || 'development'
-  , argv = torero.argv
-  , config = require('./app/config/' + env)
-  , app = torero.createApp(__dirname, config, {})
-  , port = argv.port || process.env.PORT || 3000
+var torero = require('torero'),
+    env = process.env.NODE_ENV || 'development',
+    argv = torero.argv,
+    config = require('./app/config/' + env),
+    app = torero.createApp(__dirname, config, {}),
+    port = argv.port || process.env.PORT || 3000;
 
 // Register the torero cache helper.
-app.registerHelper('Cache', torero.helpers.CacheHelper)
+app.registerHelper('Cache', torero.helpers.CacheHelper);
 
 app.configure(function () {
 
-  app.set('view engine', 'html')
-  app.register('.html', torero.engine)
+  app.set('view engine', 'html');
+  app.register('.html', torero.engine);
 
   // Use the cache helper's no-cache middleware.
-  app.use(app.getHelper('Cache').auditHeadersMiddleware)
-  app.use(app.getHelper('Cache').noCacheMiddleware)
+  app.use(app.getHelper('Cache').auditHeadersMiddleware);
+  app.use(app.getHelper('Cache').noCacheMiddleware);
 
-  app.use(torero.cookieParser())
-  app.use(torero.session({secret: 'boosh'}))
+  app.use(torero.cookieParser());
+  app.use(torero.session({secret: 'boosh'}));
+  app.use(torero.query());
 
-  // TODO: Add JSON body parser middleware
-  app.use(app.requestDecorator())
-  app.use(app.preRouter())
-})
+  app.use(app.requestDecorator());
+  app.use(app.preRouter());
+  app.use(torero.bodyParser());
+});
 
 app.configure('development', function () {
-  app.use(torero.errorHandler({ dumpExceptions: true, showStack: true }))
+
+  app.use(torero.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+
   app.set('soy options', {
-    eraseTemporaryFiles: true
-    , allowDynamicRecompile: true
-  })
-})
+    eraseTemporaryFiles: true,
+    allowDynamicRecompile: true
+  });
+
+});
 
 app.configure('production', function () {
-  app.use(torero.errorHandler())
-})
+  app.use(torero.errorHandler());
+});
 
 app.configure(function () {
-  app.use(app.router({}))
-})
+  app.use(app.router({}));
+});
 
 app.prefetch()
 app.mount()
