c1d246b307d83e0ef7972925dd4598e83c8c6b67
David Byrd <david@byrdhou.se>
2012-11-23 00:55:47 -1000

    updated server.js to have stylus & file parsing

--- src/all/server.js
+++ src/all/server.js
@@ -3,6 +3,8 @@ var torero = require('torero'),
     argv = torero.argv,
     config = require('./app/config/' + env),
     app = torero.createApp(__dirname, config, {}),
+    stylus = require('stylus'),
+    nib = require('nib'),
     port = argv.port || process.env.PORT || 3000;
 
 // Register the torero cache helper.
@@ -13,20 +15,32 @@ app.configure(function () {
   app.set('view engine', 'html');
   app.register('.html', torero.engine);
 
-  //use less
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
+  app.use(stylus.middleware({
+    src: __dirname + '/app/public',
+    compile : compile
+  }));
 
+  function compile(str, path) {
+    return stylus(str)
+      .define('url', stylus.url({
+        paths : [__dirname + "/app/public/stylesheets"],
+        limit : 10000
+      }))
+      .set('filename', path)
+      .set('compress', true)
+      .use(nib());
+  }
   // Use the cache helper's no-cache middleware.
   app.use(app.getHelper('Cache').auditHeadersMiddleware);
   app.use(app.getHelper('Cache').noCacheMiddleware);
 
   app.use(torero.cookieParser());
-  app.use(torero.session({secret: 'boosh'}));
+  app.use(torero.session({secret: 'america'}));
   app.use(torero.query());
 
   app.use(app.requestDecorator());
   app.use(app.preRouter());
-  app.use(torero.bodyParser({ uploadDir: process.env.TMP || process.env.TMPDIR || process.env.TEMP || __dirname + 'app/public/images/tmp' || process.cwd() }));
+  app.use(torero.multipart({ uploadDir: process.env.TMP || process.env.TMPDIR || process.env.TEMP || __dirname + 'app/public/images/tmp' || process.cwd() }));
 });
 
 app.configure('development', function () {
@@ -40,7 +54,7 @@ app.configure('development', function () {
     eraseTemporaryFiles: true,
     allowDynamicRecompile: true
   });
-  
+
 });
 
 app.configure('production', function () {
