a62d46239e0d81d483c5ac719eff74d45fb3da10
David Byrd <david@byrdhou.se>
2012-11-23 00:57:33 -1000

    added stubbed tests file

---
+++ src/all/tests.js
@@ -0,0 +1,8 @@
+module.exports = function (app, config) {
+  console.log('running tests ... ');
+  /*
+    You can put whatever type of testing framework 
+    you want in here. You will have access to the app 
+    variable and all of the configs specified in 
+  */
+};
